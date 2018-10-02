6851e1c2aa4c14e82403a6156206dd9c17eeb693
jaderosse <jademrosse@gmail.com>
2018-10-01 19:17:06 -0700

    added some notes for startboard steps

--- client/src/Board.js
+++ client/src/Board.js
@@ -4,16 +4,16 @@ import Socket from './socket.js';
 
 class Board extends Component {
 	render(){
-		let cells = [];
+	let cells = [];
   	for (let i = 1; i <= 9; i++) {
     	cells.push(<Socket value={i} />);
   	}
-			return (
-				<div>
-					{cells}
-				</div>
-			)
-		}
+		return (
+			<div>
+				{cells}
+			</div>
+		)
 	}
+}
 
 export default Board;
--- client/src/startBoard.js
+++ client/src/startBoard.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import Socket from './socket';
+
+//have link show up after n selections
+//onclick function appends e.target to board then to socket
 
 class StartBoard extends Component {
 	render(){
-		return(
-			<div>
-				<h1>This is the board where it starts</h1>
-				<Link to="/game">Start</Link>
-			</div>
+	return(
+		<div>
+			<h1>each one i guess</h1>
+			<Link to="/game">Start</Link>
+		</div>
 		)
 	}
 }
